73758cdc012c5c1d4936b4832bb80858e3657884
Umair Khan <m.umairkhan.co@proton.me>
2023-12-17 18:38:11 +0500

    added basic navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
+      <Link href="/user">User</Link>
+      <Link href="/user/new">New-User</Link>
     </main>
   );
 }
---
+++ app/user/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const User = () => {
+  return (
+    <>
+      <div>User</div>
+      <Link href="/">Home</Link>
+    </>
+  );
+};
+
+export default User;
