40a738a80518524667f1b2231f73cdaf271528ac
Umair Khan <m.umairkhan.co@proton.me>
2023-12-18 20:11:00 +0500

    server side data fetching

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link";
 import React from "react";
 
-const User = () => {
+const User = async () => {
+  interface User {
+    name: string;
+    id: number;
+  }
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data: User[] = await res.json();
   return (
     <>
       <div>User</div>
       <Link href="/">Home</Link>
+      {data.map((user) => {
+        return <h1 key={user.id}>{user.name}</h1>;
+      })}
     </>
   );
 };
